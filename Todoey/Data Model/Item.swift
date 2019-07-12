6537238157182d678af7f2af320a214f46bbc4c5
Felipe Minotta <felipe.minotta@upr.edu>
2019-07-12 19:58:59 -0400

    Persisting data using codable protocol and plist.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Felipe Minotta on 7/11/19.
+//  Copyright © 2019 Felipe Minotta. All rights reserved.
+//
+
+import Foundation
+
+
+class Item: Codable {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
